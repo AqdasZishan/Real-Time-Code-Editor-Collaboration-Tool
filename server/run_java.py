d29feea4a1ad32acab3827bd8c7e6a5e1812bfc6
Aqdas Zishan <97790386+AqdasZishan@users.noreply.github.com>
2024-03-25 15:35:09 +0530

    Update run_java.py

--- server/run_java.py
+++ server/run_java.py
@@ -6,7 +6,8 @@ def extract_class_name(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
         for i, line in enumerate(lines):
-            if 'public static void main(String[] args)' in line:
+            # Check for both valid main method signatures
+            if 'public static void main(String[] args)' in line or 'public static void main(String args[])' in line:
                 # Look for the class declaration line before the main method
                 for j in range(i-1, -1, -1): # Reverse search from the current line
                     class_line = lines[j].strip()
@@ -17,6 +18,7 @@ def extract_class_name(filename):
                         # Check if the class name ends with a brace and remove it if so
                         if class_name.endswith('{'):
                             class_name = class_name[:-1]
+                        print(class_name)
                         return class_name
     return None
 
@@ -55,7 +57,7 @@ def main():
 
     except Exception as e:
         print(f"Error executing code: {e}")
-
+    
     # executes only when the classpath is found
     if(classpath):
         # Remove all .class files in the directory and its subdirectories
