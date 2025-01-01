99c1f2e8a54686967d9327fa334893cc85a2e054
Aqdas Zishan <97790386+AqdasZishan@users.noreply.github.com>
2025-01-01 23:40:27 +0530

    Add files via upload

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,32 @@
 const codeEditor = ace.edit("code-editor");
 codeEditor.setTheme("ace/theme/twilight");
-codeEditor.session.setMode("ace/mode/python");
+//codeEditor.session.setMode("ace/mode/python");
 
 const languageSelect = document.getElementById('language-select');
+
+// Function to update code editor mode and highlighter
+function updateCodeEditorMode(newLanguage) {
+    // Change the mode
+    codeEditor.setOption("mode", `ace/mode/${newLanguage.toLowerCase()}`);
+    
+    // Update the highlighter
+    codeEditor.getSession().setMode(`ace/mode/${newLanguage.toLowerCase()}`);
+    
+    // Refresh the editor
+    codeEditor.resize();
+}
+
+// Initial setup
+updateCodeEditorMode('python');
+
+// Event listener for language select
+languageSelect.addEventListener('change', function() {
+    const selectedLanguage = this.value;
+    
+    // Update the code editor mode and highlighter
+    updateCodeEditorMode(selectedLanguage);
+});
+
 const runButton = document.getElementById('run-button');
 
 runButton.addEventListener('click', () => {
@@ -26,6 +50,6 @@ ws.onopen = function() {
 
 ws.onmessage = function(event) {
     console.log('Received from server:', event.data);
-    const outputDiv = document.getElementById('output');
+    const outputDiv = document.getElementById('view');
     outputDiv.textContent = event.data;
 };
